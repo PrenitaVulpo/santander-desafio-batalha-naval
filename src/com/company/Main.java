79cba16200162302bc0156e49ebf2820f16b6d81
Samuel Giacomelli Bruing <sgbruing@gmail.com>
2022-01-13 00:23:29 -0300

    Add checkWinner function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -39,14 +39,28 @@ public class Main {
     };
     private static int turn = 2;
     private static String player = "";
-    private static int player1Ships = 8;
+    private static int player1Ships = 0;
     private static int player2Ships = 0;
     static Random generateRandon = new Random();
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
+        System.out.println("----------------------------------");
+        System.out.println("| Bem vindo ao Batalha Naval ☠️|");
+        System.out.println("----------------------------------");
+
+        //inserir scanner para nome do usuario
+
+        startGame();
+
+    }
+
+    private static void startGame() {
         //Posicionamento das peças no tabuleiro
+
+        System.out.println("- Inicialmente, iremos posicionar as embarações -");
+
         while (player1Ships <= 10 && player2Ships <= 10) {
 
             placeShips();
@@ -59,7 +73,6 @@ public class Main {
         }
 
         startCombat();
-
     }
 
     private static void showTable() {
@@ -140,6 +153,7 @@ public class Main {
                 //Tiro certeiro
                 tablePlayer2[coordinates[0]][coordinates[1]] = "*";
                 System.out.println("Tiro certeiro!");
+                player2Ships--;
                 return true;
             }
 
@@ -154,6 +168,7 @@ public class Main {
                 //Tiro certeiro
                 tablePlayer1[coordinates[0]][coordinates[1]] = "*";
                 System.out.println("Tiro certeiro!");
+                player1Ships--;
                 return true;
             }
 
@@ -165,7 +180,7 @@ public class Main {
 
     public static void startCombat() {
 
-        System.out.println("O combate começou!");
+        System.out.println("---> O combate começou! <---");
 
         while (Objects.equals(checkWinner(), "")) {
 
@@ -198,15 +213,15 @@ public class Main {
                 System.out.println("Jogada do Bot");
 
                 int coordinates[] = new int[2];
-                coordinates[0] = generateRandon.nextInt(9);
-                coordinates[1] = generateRandon.nextInt(9);
-                System.out.println("Cordenada do bot -> " + coordinates[0] + ", " + coordinates[1]);
+                coordinates[0] = generateRandon.nextInt(9); //Para testar é só reduzir o range do bot
+                coordinates[1] = generateRandon.nextInt(9);//Para testar é só reduzir o range do bot
+                //System.out.println("Cordenada do bot -> " + coordinates[0] + ", " + coordinates[1]);
 
                 while(!shoot(coordinates)) {
 
                     coordinates[0] = generateRandon.nextInt(9);
                     coordinates[1] = generateRandon.nextInt(9);
-                    System.out.println("Cordenada do bot -> " + coordinates[0] + ", " + coordinates[1]);
+                    //System.out.println("Cordenada do bot -> " + coordinates[0] + ", " + coordinates[1]);
 
                 }
 
@@ -216,18 +231,18 @@ public class Main {
 
         }
 
-        System.out.println("O vencedor é "+checkWinner());
+        System.out.println("O vencedor é o(a): "+checkWinner());
 
     }
 
     private static String checkWinner(){
-        /*if (player1Ships == 0 && turn > 4) {
-            return "Jogador 2";
+        if (player1Ships == 0 && turn > 4) {
+            return "Bot";
         }
 
         if (player2Ships == 0 && turn > 4) {
             return "Jogador 1";
-        }*/
+        }
 
         return "";
     }
